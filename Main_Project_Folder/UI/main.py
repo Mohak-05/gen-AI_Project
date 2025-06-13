2e95462d6b38aeef391b17e3a499afc560a616f3
Nandu <nandumahesh2004@gmail.com>
2025-06-13 12:51:59 +0530

    beta model

--- Main_Project_Folder/UI/main.py
+++ Main_Project_Folder/UI/main.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 import streamlit as st
 from App.generator import generate_character
 
